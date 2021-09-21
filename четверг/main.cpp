c10da069826d638552848d572df8deebfce951ce
solo <getright>
2021-09-21 23:10:30 +0200

    Initial Commit

---
+++ четверг/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  четверг
+//
+//  Created by Максим Хамицкий on 21.09.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
